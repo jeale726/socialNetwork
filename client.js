d302ee84eb283c3c279afed4f6dfdc08de53b562
fperezcastro1 <franciscoperezcastro1@gmail.com>
2018-01-23 09:35:29 +0100

    step2 and beginning of step3

--- client.js
+++ client.js
@@ -4,8 +4,40 @@ displayView = function(){
 window.onload = function(){
    //code that is executed as the page is loaded.
    //You shall put your own custom code here.
-​
-	document.getElementById("body").innerHTML= "hello";
-//window.alert() is not allowed to be used in your implementation.
-    window.alert("Hello TDDD97!");
+   //document.getElementById("body").innerHTML= "hello";
+   //window.alert() is not allowed to be used in your implementation.
+   //window.alert("Hello TDDD97!");
+
+    function validateForm() {
+
+	    var password = document.getElementById("password");
+	    var repeatPassword = document.getElementById("repeatPassword");
+
+	    if (password.value != repeatPassword.value){
+	    	password.focus();
+	    	window.alert("they are the same!");
+	    	//return false;
+	    }
+	    else{
+	    	window.alert("they are NOT the same!");
+	    	//return true;
+	    }
+	}
+
+	/*
+    $('#login-btn').on("click",function(){
+
+		var div_id = (parseInt($(this).attr("id"))).toString();
+		var clase = $(this).attr("class");
+		var opt;
+
+		if(clase == "grid-item con-sombrita cont"){
+			opt = select1.find('option[value="'+ div_id +'"]');
+		}
+		else{
+			opt = select2.find('option[value="'+ div_id +'"]');
+		}
+	});
+	*/
+
 };
